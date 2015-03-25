ea63d020e42c9a46ef62a241c5e381b1602267ac
Anderson Juhasc <anderson.juhasc@gmail.com>
2015-03-24 22:03:06 -0300

    add meta viewport

--- partials/header.php
+++ partials/header.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 	<title>Cotação Bitcoin</title>
     <link rel="stylesheet" href="assets/css/style.css" type="text/css" />
 </head>
